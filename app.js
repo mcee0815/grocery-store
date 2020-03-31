9b37549f2d58452a53006521b740708173635303
mike <mikecee0815@gmail.com>
2020-03-30 20:37:48 -0400

    stock info is viewable in the browser

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 // the data.....
-let groceries = [
-    {brand:'Goya',desc:'black beans',type:'canned goods',quantity:23,price:.89},
-    {brand:'uncle bens',desc:'16oz boxed rice',type:'box goods',quantity:43,price:1.99},
-    {brand:'tuscan',desc:'milk',type:'diary',quantity:73,price:3.99},
-]
- // save to local storage
- localStorage.setItem('stock',JSON.stringify(groceries))
+// let groceries = [
+//     {brand:'Goya',desc:'black beans',type:'canned goods',quantity:23,price:.89},
+//     {brand:'uncle bens',desc:'16oz boxed rice',type:'box goods',quantity:43,price:1.99},
+//     {brand:'tuscan',desc:'milk',type:'diary',quantity:73,price:3.99},
+// ]
+//  // save to local storage
+//  localStorage.setItem('stock',JSON.stringify(groceries))
 
 
 //  get the data
-let stock = localStorage.getItem('stock')
-
-
+let stockJSON = localStorage.getItem('stock')
+let stock = JSON.parse(stockJSON)
 
 // display the data
-console.log(JSON.parse(stock))
+stock.forEach(el => {
+    // make a dom element for the data
+    let stockInfo = document.createElement('p')
+    
+    stockInfo.textContent  = `Brand:${el.brand},  Desc:'${el.desc}',  Quantity:${el.quantity}`
+    
+    document.querySelector('#stock-info').appendChild(stockInfo)
+    
+
+});
